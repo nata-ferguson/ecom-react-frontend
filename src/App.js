4c44ca7f7afe3434344ca5a82f857a1699db12e6
Natasha Ferguson <nata.ferguson@outlook.com>
2023-11-01 14:59:56 -0500

    added add to cart feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React, { useState } from "react";
 import "./App.css";
 import ProductList from "./ProductList";
 import products from "./productsData";
 
 function App() {
+  //define the shopping cart state
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
   return (
     <div className="App">
-      <ProductList products={products} />
+      {/* products data passed down to productList component as a prop products */}
+      <ProductList products={products} addToCart={addToCart} />
+      <div className="cart">
+        <h2>Shopping Cart</h2>
+        <ul>
+          {cart.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/ProductList.js
+++ src/ProductList.js
@@ -2,10 +2,16 @@ import React from "react";
 import Product from "./Product";
 
 export default function ProductList(props) {
+  const addToCart = (product) => {
+    //update the cart state by adding the selected product
+    props.addToCart(product);
+  };
   return (
     <div className="product-list">
       {props.products.map((product) => (
-        <Product key={product.id} product={product} />
+        // product={product} is where the individual product object from
+        // the array of products is being passed down to each Product component
+        <Product key={product.id} product={product} addToCart={addToCart} />
       ))}
     </div>
   );
